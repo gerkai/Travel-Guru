23464532156befddaf81d56e0c1b580492a80a86
gerkai <gerkai202@gmail.com>
2020-09-16 21:03:24 +0600

    TRAVEL GURU project start

---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import { Link } from '@material-ui/core';
+import React from 'react';
+import './Header.css';
+
+import header from '../Images/Rectangle 1.png';
+import logo from '../Images/Logo.png';
+const Header = () => {
+    return (
+        <div style={{ backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(${header})` }} className="header">
+            <nav className="nav">
+                <ul>
+                    <li>
+                        <img className="logo" src={logo} alt=""/>
+                    </li>
+                    <li>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link className="btn-book" to="/book">Book</Link>
+                    </li>
+                </ul>
+            </nav>
+           
+        </div>
+    );
+};
+
+export default Header;
