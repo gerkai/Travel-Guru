7254526066348d736e88eed35a87a5ca808d6f1b
gerkai <gerkai202@gmail.com>
2020-09-25 21:37:05 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
+import Home from "./components/Home/Home"; 
 import Login from "./components/Login/Login";
 import Book from "./components/Book/Book";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
